9c2362703a01ca4cbeab3dba30a456e6acd0c3ef
Varun <varun@cyces.co>
2018-05-25 16:55:54 +0530

    first commit

--- ottodc/bookingapp/admin.py
+++ ottodc/bookingapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from bookingapp.models import Brand, Category
+
+admin.site.register(Brand)
+admin.site.register(Category)
--- ottodc/bookingapp/models.py
+++ ottodc/bookingapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    Name = models.TextField(null=False)
+
+    def __str__(self):
+        return self.Name
+
+
+class Brand(models.Model):
+    Name = models.TextField(null=False)
+    Categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.Name
+
